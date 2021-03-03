8c10c96fe6c2f82e036395cd7602bfe1d81f5bb6
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-03-03 18:41:54 -0300

    DevsNotes API: Adicionar um dado

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -39,4 +39,32 @@ class NoteController extends Controller
 
         return $this->array;
     }
+
+    public function new(Request $request)
+    {
+
+        $title = $request->input('title');
+        $body = $request->input('body');
+
+        if ($title && $body) {
+
+            $note = new Note();
+
+            $note->title = $title;
+            $note->body = $body;
+
+            $note->save();
+
+            $this->array['result'] = [
+                'id' => $note->id,
+                'title' => $title,
+                'body' => $body
+            ];
+
+        } else {
+            $this->array['error'] = 'Campos não enviados';
+        }
+
+        return $this->array;
+    }
 }
